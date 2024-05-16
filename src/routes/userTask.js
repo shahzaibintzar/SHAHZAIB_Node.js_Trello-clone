9e40a8b72fed82781f5735aabf11a9e3cc49a984
SHAHZAIB <shahzaibintzar27346@gmail.com>
2024-05-16 10:01:14 +0500

    Task Update And Delete By Id

--- src/routes/userTask.js
+++ src/routes/userTask.js
@@ -40,6 +40,64 @@ router.get('/getTask', async (req, res) => {
 })
 
 
+// get tasks by id
+
+router.get('/getTaskById/:id', async (req, res) => {
+    try {
+        const userId = req.params.id
+        const taskData = await Task.findById(userId);
+
+        if(!taskData){
+            return res.status(404).json({message: 'Task not found'});
+        }
+        res.status(201).json({message: 'User saved successfully ', taskData});
+    } catch (error) {
+        return res.status(500).json({error : error.message});
+    }
+})
+// update tasks by id
+
+router.put("/updateTaskById/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const updatedTask = {
+            title: req.body.title,
+            description: req.body.description,
+            category: req.body.category,
+            dueDate: req.body.dueDate,
+            priority: req.body.priority,
+        };
+        const updateData = await Task.findByIdAndUpdate(taskId, updatedTask, {
+            new: true,
+        });
+
+        if (!updateData) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+
+        res.status(200).json({ message: 'Task updated successfully', updateData });
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred', error: error.message });
+    }
+});
+
+// delete tasks by id
+
+router.delete("/deleteTaskById/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const deleteData = await Task.findByIdAndDelete(taskId);
+
+        if (!deleteData) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+
+        res.status(200).json({ message: 'Task deleted successfully', deleteData });
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred', error: error.message });
+    }
+});
+
 
 
 module.exports = router
